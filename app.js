94d1b1046fc40631f7e06c67ae871cb1321b419a
anrypwnz <anrypwnz@gmail.com>
2020-06-26 14:45:09 +0300

    fix: after 2nd review

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const path = require('path');
 const cards = require('./routes/cards.js');
@@ -6,20 +7,11 @@ const users = require('./routes/users.js');
 
 const { PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
-
+app.listen(PORT);
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/cards', cards);
-app.get('/cards/:any', (req,res) => {
-  res.status(404).send({ 'message': 'Запрашиваемый ресурс не найден' });
-});
-app.get('/users', users);
-app.get('/users/:id', users);
-
-app.all('/:any', (req,res,next)=> {
+app.use(cards);
+app.use(users);
+app.all('*', (req, res, next) => {
   res.status(404).send({ 'message': 'Запрашиваемый ресурс не найден' });
   next();
 });
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
+
 const usersData = path.join(__dirname, '../data/users.json');
 
 router.get('/users', (req, res) => {
-  fs.readFile(usersData,{ encoding: 'utf8' }, (err, data) => {
-    if(err){
+  fs.readFile(usersData, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
       console.log(err);
       return;
     }
-   res.send(data);
+    res.send(data);
   });
 });
 
-router.get('/users/:id', (req,res) => {
-  fs.readFile(usersData,{ encoding: 'utf8' }, (err, data) => {
-    if(err){
+router.get('/users/:id', (req, res) => {
+  fs.readFile(usersData, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
       console.log(err);
       return;
     }
     const inData = JSON.parse(data);
-    let resUser = inData.find(item => item._id === req.params.id);
+    const resUser = inData.find((item) => item._id === req.params.id);
     if (resUser) {
       res.send(resUser);
     } else {
